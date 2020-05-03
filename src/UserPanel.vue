3b2468c44522e213727c8ed13cc278e76688f0cf
Paweł Tybinka <tybinka@student.agh.edu.pl>
2020-05-03 22:10:44 +0200

    done

--- src/UserPanel.vue
+++ src/UserPanel.vue
@@ -2,11 +2,20 @@
     <div>
         <h2>Witaj {{ username }}</h2>
         <a @click="$emit('logout')">Wyloguj</a>
+        <br>
     </div>
 </template>
 
 <script>
+    import MeetingsPage from "./meetings/MeetingsPage.vue";
+
     export default {
-        props: ['username']
+        components: { MeetingsPage },
+        props: ['username'],
+        methods: {
+            addNewMeeting(meeting) {
+                this.meetings.push("nowe spotkanie");
+            }
+        }
     }
 </script>
